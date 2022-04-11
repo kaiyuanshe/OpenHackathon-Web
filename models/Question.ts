9104f097abd111bdfa41f92f9e33ce38ed043163
Soecka <twcbnubio@gmail.com>
2022-04-11 17:26:22 +0800

    optimization for Register Page [trim] use bootstrap Form component [trim] button position,react key,Eslint check [add]  extract Question.ts for model files [add] form handleSubmit [add] passHref for Link,location.href [trim] remove return,use ms-2 for spacing

---
+++ models/Question.ts
@@ -0,0 +1,79 @@
+export interface Question {
+  title: string;
+  options?: string[];
+  multiple?: boolean;
+  type?: 'text' | 'url';
+  required?: boolean;
+}
+
+export interface Extensions {
+  name: string;
+  value: string;
+}
+export const questions: Question[] = [
+  {
+    title: '您的专业',
+    options: [
+      '前端工程师',
+      '后端工程师',
+      '客户端工程师',
+      '游戏工程师',
+      '算法工程师',
+      '区块链工程师',
+      '运维工程师',
+      '测试工程师',
+      '架构师',
+      '项目/产品经理',
+      '在校生',
+      '其它',
+    ],
+    multiple: true,
+  },
+  {
+    title: '常用的编程语言',
+    options: [
+      'JavaScript/TypeScript',
+      'Java/Scala/Groovy/Kotlin',
+      'C#',
+      'Python',
+      'PHP',
+      'Ruby',
+      'Dart',
+      'Objective-C/Swift',
+      'Rust',
+      'Go',
+      'C/C++',
+      '其它',
+    ],
+    multiple: true,
+  },
+  {
+    title: '领英/个人简历/个人博客',
+    type: 'url',
+  },
+  {
+    title: '社交媒体账号/推特/微博',
+    type: 'url',
+  },
+  {
+    title: '本次比赛您感兴趣的赛题是什么？',
+    type: 'text',
+  },
+  {
+    title: '您希望从系列工作坊/讲座中听到哪方面内容',
+    type: 'text',
+  },
+  {
+    title: '您是否愿意进行组队？',
+    options: ['是', '否，喜欢个人', '否，已有队伍'],
+  },
+  {
+    title: '您介意新手加入您的团队？',
+    options: ['是', '否'],
+  },
+  {
+    title:
+      '关于队友，您还有什么其他需求吗? 我们会尽全力帮助您找到合适人选，助力您的黑客松之旅。',
+    type: 'text',
+  },
+];
