d00f0b9f93be80acafe0b28331635b7498bda740
Shakalakab <yunjie989@gmail.com>
2022-05-12 14:18:37 +0800

    Add logout in AuthContext

--- components/UserBar.tsx
+++ components/UserBar.tsx
@@ -1,10 +1,8 @@
-import { PureComponent, useContext } from 'react';
-import { Button, Dropdown, Nav } from 'react-bootstrap';
+import { PureComponent } from 'react';
+import { Button, Dropdown } from 'react-bootstrap';
 import Link from 'next/link';
 
 import { User } from '../models/User';
-import { request } from '../pages/api/core';
-import { getClientSession } from '../pages/api/user/session';
 import { AuthContext } from '../context/AuthContext';
 
 interface State {
@@ -15,21 +13,7 @@ export class UserBar extends PureComponent<{}, State> {
   state: Readonly<State> = {};
   static contextType = AuthContext;
 
-  // async componentDidMount() {
-  //   try {
-  //     const user = await getClientSession();
-  //     this.setState({ user });
-  //   } catch {}
-  // }
-
-  async signOut() {
-    await request('user/session', 'DELETE');
-
-    location.replace('/');
-  }
-
   render() {
-    // const { user } = this.state;
     const user = this.context.user;
 
     return !user ? (
@@ -45,7 +29,7 @@ export class UserBar extends PureComponent<{}, State> {
           <Dropdown.Toggle>{user.nickname}</Dropdown.Toggle>
           <Dropdown.Menu>
             <Dropdown.Item href={`/user/${user.id}`}>个人主页</Dropdown.Item>
-            <Dropdown.Item onClick={this.signOut}>登出</Dropdown.Item>
+            <Dropdown.Item onClick={this.context.logout}>登出</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </>
