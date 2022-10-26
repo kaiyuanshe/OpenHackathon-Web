fb9a98e593fd8c546510682ded83e2dbd7907cf3
Leo <leo@wangrunlin.com>
2022-10-26 15:46:56 +0800

    [feat] activity Organization info

---
+++ components/Organization/OrganizationCard.tsx
@@ -0,0 +1,20 @@
+import { Card } from 'react-bootstrap';
+
+import { Organization, OrganizationTypeName } from '../../models/Organization';
+
+export const OrganizationCard = ({
+  name,
+  description,
+  type,
+  logo,
+}: Organization) => (
+  <Card>
+    <Card.Img src={logo?.uri} alt={logo?.name} title={logo?.name} />
+    <Card.Body>
+      <Card.Title>{name}</Card.Title>
+      <Card.Subtitle>{OrganizationTypeName[type]}</Card.Subtitle>
+      <hr></hr>
+      <Card.Text>{description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
