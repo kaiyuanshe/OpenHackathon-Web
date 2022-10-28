28cfa79ee0ced959b37c8c94478f8d8011f7cfec
Leo <leo@wangrunlin.com>
2022-10-28 15:55:11 +0800

    feat: udpate OrganizationCardList style

--- components/Organization/OrganizationList.tsx
+++ components/Organization/OrganizationList.tsx
@@ -21,7 +21,7 @@ export class OrganizationCardList extends ScrollList<OrganizationListProps> {
   static Layout = ({ value = [] }: OrganizationListProps) => (
     <div>
       {value.map(item => (
-        <Row key={item.id}>
+        <Row key={item.id} style={{ padding: '.5rem' }}>
           <OrganizationCard {...item} />
         </Row>
       ))}
