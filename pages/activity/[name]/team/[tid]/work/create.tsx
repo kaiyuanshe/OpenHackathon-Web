6ccaeac85a5783995843cc4c6e4bc2f5608cb4fa
Guanngxu <446678850@qq.com>
2023-01-02 03:14:08 +0800

    fix: build bug

--- pages/activity/[name]/team/[tid]/work/create.tsx
+++ pages/activity/[name]/team/[tid]/work/create.tsx
@@ -2,10 +2,12 @@ import type { InferGetServerSidePropsType } from 'next';
 
 import PageHead from '../../../../../../components/PageHead';
 import { WorkEdit } from '../../../../../../components/work/WorkEdit';
+import { i18n } from '../../../../../../models/Translation';
 import { withRoute } from '../../../../../api/core';
-import { i18n } from '../models/Translation';
 
 export const getServerSideProps = withRoute<Record<'name' | 'tid', string>>();
+
+const { t } = i18n;
 
 export default function WorkCreatePage({
   route: { params },
