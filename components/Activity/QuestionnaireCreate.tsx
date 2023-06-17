3fb7a3c73c7e755ccc058858a2ffdcc2df363983
South Drifted <shiy2008@gmail.com>
2023-06-17 22:35:38 +0800

    Update components/Activity/QuestionnaireCreate.tsx

--- components/Activity/QuestionnaireCreate.tsx
+++ components/Activity/QuestionnaireCreate.tsx
@@ -40,62 +40,60 @@ export const QuestionnaireCreate: FC<QuestionnaireCreateProps> = observer(
     };
 
     return (
-      <div className="container-fluid">
-        <Form onSubmit={handleSubmit}>
-          <Row as="ul" className="list-unstyled w-100 align-items-end">
-            <Col as="li" sm={4}>
-              <Form.Group className="mb-2 w-20" controlId="id">
-                <Form.Label>
-                  {t('question_id')}
-                  {t('quote_required')}
-                </Form.Label>
-                <Form.Control name={'id'} required />
-              </Form.Group>
-            </Col>
-            <Col as="li" sm={8}>
-              <Form.Group className="mb-2 flex-grow-1" controlId="title">
-                <Form.Label>
-                  {t('question_description')}
-                  {t('quote_required')}
-                </Form.Label>
-                <Form.Control name={'title'} required />
-              </Form.Group>
-            </Col>
-            <Col as="li" sm={4}>
-              <Form.Group className="mb-2" controlId="type">
-                <Form.Label>{t('question_type')}</Form.Label>
-                <Form.Select name="type">
-                  <option value="text">{t('text')}</option>
-                  <option value="url">{t('link')}</option>
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col as="li" sm={8}>
-              <Form.Group className="mb-2" controlId="options">
-                <Form.Label>{t('option_config')}</Form.Label>
-                <BadgeInput name="options" placeholder={t('tag_placeholder')} />
-              </Form.Group>
-            </Col>
-            <Col as="li" sm={2}>
-              <Form.Group className="mb-2" controlId="multiple">
-                <Form.Label>{t('whether_multiple')}</Form.Label>
-                <Form.Check className="mx-2" type="switch" name="multiple" />
-              </Form.Group>
-            </Col>
-            <Col as="li" sm={2}>
-              <Form.Group className="mb-2" key="required">
-                <Form.Label>{t('whether_required')}</Form.Label>
-                <Form.Check className="mx-2" type="switch" name="required" />
-              </Form.Group>
-            </Col>
-            <Col sm={8} className="text-end">
-              <Button className="px-5 mb-2" type="submit" variant="success">
-                {t('add_question')}
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-      </div>
+      <Form className="container-fluid" onSubmit={handleSubmit}>
+        <Row as="ul" className="list-unstyled w-100 align-items-end g-3">
+          <Col as="li" sm={4}>
+            <Form.Group className="w-20" controlId="id">
+              <Form.Label>
+                {t('question_id')}
+                {t('quote_required')}
+              </Form.Label>
+              <Form.Control name="id" required />
+            </Form.Group>
+          </Col>
+          <Col as="li" sm={8}>
+            <Form.Group className="flex-grow-1" controlId="title">
+              <Form.Label>
+                {t('question_description')}
+                {t('quote_required')}
+              </Form.Label>
+              <Form.Control name="title" required />
+            </Form.Group>
+          </Col>
+          <Col as="li" sm={4}>
+            <Form.Group controlId="type">
+              <Form.Label>{t('question_type')}</Form.Label>
+              <Form.Select name="type">
+                <option value="text">{t('text')}</option>
+                <option value="url">{t('link')}</option>
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col as="li" sm={8}>
+            <Form.Group controlId="options">
+              <Form.Label>{t('option_config')}</Form.Label>
+              <BadgeInput name="options" placeholder={t('tag_placeholder')} />
+            </Form.Group>
+          </Col>
+          <Col as="li" sm={2}>
+            <Form.Group controlId="multiple">
+              <Form.Label>{t('whether_multiple')}</Form.Label>
+              <Form.Check className="mx-2" type="switch" name="multiple" />
+            </Form.Group>
+          </Col>
+          <Col as="li" sm={2}>
+            <Form.Group key="required">
+              <Form.Label>{t('whether_required')}</Form.Label>
+              <Form.Check className="mx-2" type="switch" name="required" />
+            </Form.Group>
+          </Col>
+          <Col sm={8} className="text-end">
+            <Button className="px-5 " type="submit" variant="success">
+              {t('add_question')}
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     );
   },
 );
