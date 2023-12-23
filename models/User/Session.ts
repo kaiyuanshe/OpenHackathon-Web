6eb3376eea6469a7db58b628d811ac1c6c635303
TechQuery <shiy2008@gmail.com>
2023-12-24 05:00:32 +0800

    [add] GitHub sign-in button & page with Strapi API [add] Lark notification for Vercel preview URL [optimize] Layout & Style of Team Work card & User Detail page

--- models/User/Session.ts
+++ models/User/Session.ts
@@ -1,12 +1,40 @@
 import { HTTPClient } from 'koajax';
 import { computed, makeObservable, observable } from 'mobx';
-import { setCookie } from 'mobx-i18n';
+import { parseCookie, setCookie } from 'mobx-i18n';
 import { BaseModel, toggle } from 'mobx-restful';
 import { buildURLData, sleep } from 'web-utility';
 
 import { AuthingIdentity, AuthingUserBase, User } from '.';
 
-const { localStorage } = globalThis;
+const { localStorage, document } = globalThis;
+
+const { token } = (document ? parseCookie() : {}) as { token: string };
+
+export const strapiClient = new HTTPClient({
+  baseURI: `${
+    process.env.NODE_ENV === 'development'
+      ? 'http://127.0.0.1:1337'
+      : 'https://hackathon-server.kaiyuanshe.cn'
+  }/api/`,
+  responseType: 'json',
+}).use(({ request }, next) => {
+  if (token)
+    request.headers = { ...request.headers, Authorization: `Bearer ${token}` };
+
+  return next();
+});
+
+export interface Base extends Record<'createdAt' | 'updatedAt', string> {
+  id: number;
+}
+
+export interface SessionUser
+  extends Base,
+    Record<'username' | 'email', string>,
+    Record<'confirmed' | 'blocked', boolean> {
+  provider: 'local' | 'github';
+  gender?: 'Female' | 'Male' | 'Other';
+}
 
 export class SessionModel extends BaseModel {
   constructor() {
